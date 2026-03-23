ad6f13ddea8bafaaaa65399fb9f0ef08d4679b1e
ＡＬＥＸＴＷ <alextw@ALEXTWdeMac-mini.local>
2026-03-23 10:18:20 -0700

    feat: integrate FilterEngine and enable click-to-jump in results

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,7 +1,8 @@
 #include "MainWindow.h"
 #include <QToolBar>
 #include <QDockWidget>
-#include <QPushButton>
+#include <QTextCursor>
+#include <QTextBlock>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setupUI();
@@ -17,7 +18,7 @@ void MainWindow::setupUI() {
     filterInput->setPlaceholderText("Enter keywords separated by |");
     logicCombo = new QComboBox(this);
     logicCombo->addItems({"OR", "AND"});
-    QPushButton* filterBtn = new QPushButton("Filter", this);
+    filterBtn = new QPushButton("Filter", this);
 
     toolbar->addWidget(filterInput);
     toolbar->addWidget(logicCombo);
@@ -27,4 +28,34 @@ void MainWindow::setupUI() {
     resultsList = new QListWidget(this);
     dock->setWidget(resultsList);
     addDockWidget(Qt::BottomDockWidgetArea, dock);
+
+    connect(filterBtn, &QPushButton::clicked, this, &MainWindow::runFilter);
+    connect(resultsList, &QListWidget::itemDoubleClicked, this, &MainWindow::onResultDoubleClicked);
+}
+
+void MainWindow::runFilter() {
+    resultsList->clear();
+    QStringList keywords = filterInput->text().split("|", Qt::SkipEmptyParts);
+    for (int i = 0; i < keywords.size(); ++i) keywords[i] = keywords[i].trimmed();
+    
+    engine.setKeywords(keywords);
+    engine.setLogic(logicCombo->currentText() == "AND" ? FilterLogic::AND : FilterLogic::OR);
+    
+    QString text = editor->toPlainText();
+    QStringList lines = text.split("\n");
+    for (int i = 0; i < lines.size(); ++i) {
+        if (engine.matchLine(lines[i])) {
+            QListWidgetItem* item = new QListWidgetItem(QString("Line %1: %2").arg(i + 1).arg(lines[i]));
+            item->setData(Qt::UserRole, i); // Store line number (0-indexed) for safe click-to-jump
+            resultsList->addItem(item);
+        }
+    }
+}
+
+void MainWindow::onResultDoubleClicked(QListWidgetItem* item) {
+    int line = item->data(Qt::UserRole).toInt();
+    QTextCursor cursor(editor->document()->findBlockByNumber(line));
+    editor->setTextCursor(cursor);
+    editor->centerCursor();
+    editor->setFocus();
 }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -4,15 +4,22 @@
 #include <QListWidget>
 #include <QLineEdit>
 #include <QComboBox>
+#include <QPushButton>
+#include "FilterEngine.h"
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+private slots:
+    void runFilter();
+    void onResultDoubleClicked(QListWidgetItem* item);
 private:
     QPlainTextEdit* editor;
     QListWidget* resultsList;
     QLineEdit* filterInput;
     QComboBox* logicCombo;
+    QPushButton* filterBtn;
+    FilterEngine engine;
     void setupUI();
 };
