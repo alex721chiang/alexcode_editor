03006436f34ae6845e3f16608f4fad67ac094d4b
ＡＬＥＸＴＷ <alextw@ALEXTWdeMac-mini.local>
2026-03-25 22:03:45 -0700

    fix: convert icon to RGBA format and set Qt window icon to fix black icon issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <QApplication>
+#include <QIcon>
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icon.png")); // Set the application icon
     MainWindow w;
     w.show();
     return a.exec();
